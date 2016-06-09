3a0834a4f49e8c60d9a4408af4cd86624b15d7b3
yuhsh <yhshzju@163.com>
2016-06-09 22:06:58 +0800

    feature change

--- Prediction.py
+++ Prediction.py
@@ -7,6 +7,7 @@ from sklearn import linear_model
 import csv
 import time
 import math
+from sklearn.ensemble import RandomForestRegressor
 
 CURRENT_PATH = sys.path[0]#获取当前路径
 ARTIST_P_D_C = os.path.join(CURRENT_PATH, "artist_p_d_c.txt")
@@ -14,12 +15,27 @@ DAYS = 183
 START_UNIX = 1425139200#表示20150301
 DAY_SECOND = 86400#每天的秒数
 START_WEEKDAY = 7#表示是一周的第几天
+HOLIDAY = ["20150405", "20150406", "20150501", "20150502", "20150503", ...
+           , "20150620", "20150621", "20150622", "20150927", "20151001", ...
+           , "20151002", "20151003", "20151004", "20151005", "20151006", ...
+           , "20151007"]
+HOLIDAYCHANGE = ["20151010"]
 
 #线性预测函数
 def LinearPrediction(train_X, train_y, predict_x):
     regr = linear_model.Ridge(alpha=.05, fit_intercept=False)
     regr.fit(train_X, train_y)
     initial_y = regr.predict(predict_x)
+    prediction_y = []#返回的整数预测值
+    for i in range(len(initial_y)):
+        prediction_y.append(max(1, int(initial_y[i])))
+    return prediction_y
+
+#随机森林预测
+def RandomForestPrediction(train_x, train_y, predict_x):
+    rf = RandomForestRegressor()
+    rf.fit(train_x, train_y)
+    initial_y = rf.predict(predict_x)
     prediction_y = []#返回的整数预测值
     for i in range(len(initial_y)):
         prediction_y.append(max(1, int(initial_y[i])))
@@ -66,6 +82,40 @@ def CalculateF1Score(realPlayData, predictPlayData):
         F1Score += (1-sigma)*theta
     return F1Score
 
+def MakeFeature(day):
+    #feature = [day]
+    feature = []
+    #日期特征
+    weekDay = Num2WeekDay(day)
+    for i in range(1, 8):
+        if i == weekDay:
+            feature.append(1)
+        else:
+            feature.append(0)
+    #是否法定假日
+    unixTime = START_UNIX+day*DAY_SECOND
+    date = Unix2Date(unixTime)
+    '''
+    if date in HOLIDAY:
+        feature.append(1)
+    else:
+        feature.append(0)
+    '''
+    '''
+    #是否放假
+    if date in HOLIDAY:
+        feature.append(1)
+    elif date in HOLIDAYCHANGE:
+        feature.append(0)
+    elif weekDay>=6 and weekDay<=7:
+        feature.append(1)
+    else:
+        feature.append(0)
+    '''
+    #feature.append(1)
+    #feature.append(weekDay)
+    return feature
+
 if __name__ == "__main__":
     headers1 = ["ID", "Play", "Date"]
     with open(ARTIST_P_D_C) as fr:
@@ -77,44 +127,27 @@ if __name__ == "__main__":
             download = list(map(int, fr.readline().strip("\n").split(",")))
             collect = list(map(int, fr.readline().strip("\n").split(",")))
             play = np.array(play)
-            x = [[i, 1] for i in range(183)]
+            x = [MakeFeature(i) for i in range(183)]
             x = np.array(x)
-            #使用3\4月的数据训练得到predict,download,collect的预测模型
-            feature_x = x[0:61]
-            play_y = play[0:61]
-            download_y = download[0:61]
-            collect_y = collect[0:61]
-            predict_x = x[61:122]#预测的时间
-            predict_play_y = LinearPrediction(feature_x, play_y, predict_x)#得到预测的播放量
-            predict_download_y = LinearPrediction(feature_x, download_y, predict_x)#得到预测的下载量
-            predict_collect_y = LinearPrediction(feature_x, collect_y, predict_x)#得到预测的收藏量
-            #将5\6月份的数据以及预测的5,6月份的数据进行回归训练
-            final_feature_x = [[i, 1, predict_play_y[i], predict_download_y[i], predict_download_y[i]] for i in \
-                               range(len(predict_play_y))]
-            final_play_y = play[61:122]
-            #将5\6月份的数据训练得到predict,download,collect的预测模型
-            feature_x = [[i, 1] for i in range(61)]
-            play_y = play[61:122]
-            download_y = download[61:122]
-            collect_y = collect[61:122]
-            predict_x = [[i, 1] for i in range(61, 122)]
-            predict_play_y = LinearPrediction(feature_x, play_y, predict_x)#得到预测的播放量
-            predict_download_y = LinearPrediction(feature_x, download_y, predict_x)#得到预测的下载量
-            predict_collect_y = LinearPrediction(feature_x, collect_y, predict_x)#得到预测的收藏量
-            #组合测试特征
-            feature_test_x = [[predict_x[i][0], 1, predict_play_y[i], predict_download_y[i], predict_collect_y[i]] for i\
-                              in range(len(predict_x))]
-            predict_y = LinearPrediction(final_feature_x, final_play_y, feature_test_x)
-            realPlayData.append(play[122:183])
+
+            train_x = x[:-60]
+            train_y = play[:-60]
+            test_x = x[-60:]
+            test_y = play[-60:]
+            predict_y = LinearPrediction(train_x, train_y, test_x)
+            #predict_y = RandomForestPrediction(train_x, train_y, test_x)
+            realPlayData.append(test_y)
             predictPlayData.append(predict_y)
+
             '''
+            train_x = x[61:]
+            train_y = play[61:]
             predict_x = []
             for days in range(61):
-                predict_x.append([days+183, 1])
-            prediction_y = LinearPrediction(x, play, predict_x)#预测
+                predict_x.append(MakeFeature(days+183))
+            prediction_y = LinearPrediction(train_x, train_y, predict_x)#预测
             predict_file_path = os.path.join(CURRENT_PATH, "mars_tianchi_predict_data.csv")
             WriteResult(artistID, predict_file_path, prediction_y)
             '''
             artistID = fr.readline().strip("\n")
         print(CalculateF1Score(realPlayData, predictPlayData))
-        print(CalculateF1Score(realPlayData, realPlayData))
